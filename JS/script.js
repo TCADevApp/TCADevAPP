2a7b13626c16d43264f23b18ad64b8a69e97b3d0
TCADevApp <transco.application@gmail.com>
2025-01-28 01:02:09 +0100

    modify JSdatabase Info and style of home__line--list

--- JS/script.js
+++ JS/script.js
@@ -19,43 +19,43 @@ const lignesDeTransport = [
     Test: "Test",
   },
   {
-    id: 1,
-    nom: "Gare Centrale - Kingasani",
+    id: "Ligne 01",
+    nom: "Commerce - Kingasani",
     distance: "18 km",
-    busActifs: 5,
+    busActifs: "Bus Actif 05",
     arrets: [
-      { nom: "Gare Central", lien: "https://maps.google.com?q=GareCentral" },
+      { nom: "Commerce", lien: "https://maps.google.com?q=GareCentral" },
       { nom: "Kingasani", lien: "https://maps.google.com?q=Kingasani" },
     ],
   },
   {
-    id: 2,
-    nom: "Gare Central - Kintambo",
-    distance: "6 Km",
-    busActifs: 3,
-    arrets: [
-      {arret1: "Gare Central", lien: "https://maps.google.com?q=GareCentral"},
-      {arret1: "Kintambo", lien: "https://maps.google.com?q=Kitambo"},
-    ]
-  },
-  {
-    id: 3,
-    nom: "Zando - Ronp Point Ngaba",
-    distance: "15 Km",
-    busActifs: 5,
-    arrets: [
-      {arret: "Zando", lien: "https://maps.google.com?q=Zando"},
-      {arret1: "Rond Point Ngaba", lien: "https://maps.google.com?q=RondPointNgaba"},
-    ]
-  },
-  {
-    id: 4,
-    nom: "Zando - Matete",
+    id: "Ligne 02",
+    nom: "Royal - Masina Sans Fil",
     distance: "12 Km",
-    busActifs: 3,
+    busActifs: "Bus Actif 05",
     arrets: [
-      {arret: "Zando", lien: "https://maps.google.com?q=Zando"},
-      {arret1: "Matete", lien: "https://maps.google.com?q=Matete"},
+      {arret1: "Royal", lien: "https://maps.google.com?q=Royal"},
+      {arret1: "Masina Sans Fil", lien: "https://maps.google.com?q=Masina Sans Fil"},
+    ]
+  },
+  {
+    id: "Ligne 03",
+    nom: "Gare Central - Ngiri Ngiri",
+    distance: "15 Km",
+    busActifs: "Bus Actif 03",
+    arrets: [
+      {arret: "Gare Central", lien: "https://maps.google.com?q=GareCentral"},
+      {arret1: "Ngiri Ngiri", lien: "https://maps.google.com?q=NgiriNgiri"},
+    ]
+  },
+  {
+    id: "Ligne 04",
+    nom: "UPN - Gare Central",
+    distance: "12 Km",
+    busActifs: "Bus Actif 05",
+    arrets: [
+      {arret: "UPN", lien: "https://maps.google.com?q=UPN"},
+      {arret1: "Gare Central", lien: "https://maps.google.com?q=GareCentral"},
     ]
   },
   // Ajouter les 52 lignes ici
@@ -65,55 +65,55 @@ const lignesDeTransport = [
 const homeLineList = document.querySelector('.home__lines--list');
 
   //Ligne 1
-  const ligne1Nom = document.createElement('p');
-  ligne1Nom.textContent = lignesDeTransport[1].nom;
   const ligne1Id = document.createElement('p');
   ligne1Id.textContent = lignesDeTransport[1].id;
+  const ligne1Nom = document.createElement('p');
+  ligne1Nom.textContent = lignesDeTransport[1].nom;
   const ligne1Distance = document.createElement('p');
   ligne1Distance.textContent = lignesDeTransport[1].distance;
 
   const ligne1 = document.createElement('div');
-  ligne1.appendChild(ligne1Nom);
   ligne1.appendChild(ligne1Id);
+  ligne1.appendChild(ligne1Nom);
   ligne1.appendChild(ligne1Distance);
 
   //lignes 2
-  const ligne2Nom = document.createElement('p');
-  ligne2Nom.textContent = lignesDeTransport[2].nom;
   const ligne2Id = document.createElement('p');
   ligne2Id.textContent = lignesDeTransport[2].id;
+  const ligne2Nom = document.createElement('p');
+  ligne2Nom.textContent = lignesDeTransport[2].nom;
   const ligne2Distance = document.createElement('p');
   ligne2Distance.textContent = lignesDeTransport[2].distance;
 
   const ligne2 = document.createElement('div');
-  ligne2.appendChild(ligne2Nom);
   ligne2.appendChild(ligne2Id);
+  ligne2.appendChild(ligne2Nom);
   ligne2.appendChild(ligne2Distance);
 
   // Ligne 3
-  const ligne3Nom = document.createElement('p');
-  ligne3Nom.textContent = lignesDeTransport[3].nom;
   const ligne3Id = document.createElement('p');
   ligne3Id.textContent = lignesDeTransport[3].id;
+  const ligne3Nom = document.createElement('p');
+  ligne3Nom.textContent = lignesDeTransport[3].nom;
   const ligne3Distance = document.createElement('p');
   ligne3Distance.textContent = lignesDeTransport[3].distance;
 
   const ligne3 = document.createElement('div');
-  ligne3.appendChild(ligne3Nom);
   ligne3.appendChild(ligne3Id);
+  ligne3.appendChild(ligne3Nom);
   ligne3.appendChild(ligne3Distance);
 
   // Ligne 4
-  const ligne4Nom = document.createElement('p');
-  ligne4Nom.textContent = lignesDeTransport[4].nom;
   const ligne4Id = document.createElement('p');
   ligne4Id.textContent = lignesDeTransport[4].id;
+  const ligne4Nom = document.createElement('p');
+  ligne4Nom.textContent = lignesDeTransport[4].nom;
   const ligne4Distance = document.createElement('p');
   ligne4Distance.textContent = lignesDeTransport[4].distance;
 
   const ligne4 = document.createElement('div');
-  ligne4.appendChild(ligne4Nom);
   ligne4.appendChild(ligne4Id);
+  ligne4.appendChild(ligne4Nom);
   ligne4.appendChild(ligne4Distance);
 
 homeLineList.appendChild(ligne1);
